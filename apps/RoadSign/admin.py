b56b8ac9f95cacc8645fd95510bd3b37e8555f6a
shokh0126mardonov <smardonov@gamil.com>
2026-04-16 10:25:55 +0500

    done

--- apps/RoadSign/admin.py
+++ apps/RoadSign/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import RoadSign,RoadSignFolder
+
+admin.site.register([RoadSign,RoadSignFolder])
--- apps/RoadSign/models.py
+++ apps/RoadSign/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class RoadSignFolder(models.Model):    
+    name = models.CharField(max_length=255)
+    icon_path = models.ImageField(upload_to='RoadSignFolder/')
+
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'road_sign_folders'
+        indexes = [
+            models.Index(fields=['parent']),
+        ]
+
+    def __str__(self):
+        return self.name or "Unnamed Folder"
+    
+
+class RoadSign(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=50, null=True, blank=True)
+    icon_path = models.ImageField(upload_to='RoadSign/')
+    description = models.TextField(null=True, blank=True)
+
+    folder = models.ForeignKey(
+        RoadSignFolder,
+        on_delete=models.CASCADE,
+        related_name='road_signs'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        db_table = 'road_signs'
+        indexes = [
+            models.Index(fields=['folder']),
+            models.Index(fields=['code']),
+        ]
+
+    def __str__(self):
+        return self.name or "Unnamed RoadSign"
